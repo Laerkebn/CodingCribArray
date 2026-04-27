0a57721faec5f6598acc4030060a2ee5441cc7b5
Laerkebn <lbnonved@gmail.com>
2026-04-27 15:07:47 +0200

    start

---
+++ vite-project/src/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+const names = ['Lola', 'Mads', 'Søren', 'Mikkel']
+const alders = [23, 26, 35, 98]
+const Mottoer = ['Hello World', 'goodmoring sunshine', 'yellow mellow','wupti dupti']
+
+for(let i = 0; i < names.length; i++)
+{
+  const name = names[i]
+  const alder = alders[i]
+  const motto = Mottoer[i]
+  console.log(`${name} (${age} år): "${motto}"`)
+}
