a196b2edb9a978664c3e4cf36cd7dec32578814b
Laerkebn <lbnonved@gmail.com>
2026-04-27 15:30:46 +0200

    opgave 1

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import './style.css'
+import "./style.css";
 
-const names = ['Lola', 'Mads', 'Søren', 'Mikkel']
-const alders = [23, 26, 35, 98]
-const Mottoer = ['Hello World', 'goodmoring sunshine', 'yellow mellow','wupti dupti']
+const names = ["Lola", "Mads", "Søren", "Mikkel"];
+const alders = [23, 26, 35, 98];
+const mottoer = [
+  "Hello World",
+  "goodmoring sunshine",
+  "yellow mellow",
+  "wupti dupti",
+];
 
-for(let i = 0; i < names.length; i++)
-{
-  const name = names[i]
-  const alder = alders[i]
-  const motto = Mottoer[i]
-  console.log(`${name} (${age} år): "${motto}"`)
+for (let i = 0; i < names.length; i++) {
+  const name = names[i];
+  const alder = alders[i];
+  const motto = mottoer[i];
+  console.log(`${name} (${alder} år): "${motto}"`);
 }
